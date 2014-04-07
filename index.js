3f8f305b1dcf04f0febcd85e92e751a0ca6d59af
Kentaro Wakayama <wakayama.kentaro@gmail.com>
2014-04-07 23:13:24 +0200

    Refactor sort to search

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ var Imap = require('imap'),
     MailParser = require('mailparser').MailParser,
     debug = require('./utils/debug'),
     imap = null,
-    mails = [],
     env = process.env.NODE_ENV || 'development';
 
 var config = require('./env/' + env);
@@ -15,45 +14,41 @@ function openInbox(cb) {
 
 imap.once('ready', function () {
   openInbox(function (err) {
-    if (err) {
-      throw err;
-    }
+    if (err) throw err;
 
-    imap.sort([ '-ARRIVAL' ], [ 'UNSEEN' ], function (err, results) {
-      if (err) {
-        throw err;
-      }
+    imap.search([ 'UNSEEN', ['SINCE', 'May 20, 2010'] ], function(err, results) {
+      
+      if (err) throw err;
 
       if (results.length === 0) {
         debug('Nothing to fetch');
         return imap.end();
       }
+      
+      var f = imap.fetch(results, { bodies: '' });
+      
+      f.on('message', function(msg, seqno) {
 
-      var fetch = imap.fetch(results, {
-        markSeen: true,
-        bodies: ['HEADER.FIELDS (FROM SUBJECT DATE)', 'TEXT']
-      });
-
-      fetch.on('message', function (msg) {
+        var prefix = '(#' + seqno + ') ';
 
         var parser = new MailParser();
-
         parser.on('end', function (mail) {
-          mails.push(mail);
+          console.log('%s from:%s \tsubject:%s', prefix, mail.from[0].address, mail.subject);
         });
-
-        msg.on('body', function (stream) {
+        
+        msg.on('body', function(stream, info) {
           stream.pipe(parser);
         });
 
+        
       });
-
-      fetch.on('error', function (err) {
-        debug('Fetch error: ' + err);
+      
+      f.once('error', function(err) {
+        console.log('Fetch error: ' + err);
       });
-
-      fetch.on('end', function () {
-        debug('Done fetching all messages!');
+      
+      f.once('end', function() {
+        console.log('Done fetching all messages!');
         imap.end();
       });
 
@@ -67,7 +62,6 @@ imap.on('error', function (err) {
 
 imap.on('end', function () {
   debug('Connection ended');
-  debug(mails);
 });
 
 imap.connect();
